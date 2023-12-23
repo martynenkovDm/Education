5911a2ac012cfe268b23614c8a0339836ab4c6e2
martynenkovDm <126678952+martynenkovDm@users.noreply.github.com>
2023-12-23 16:23:48 +0200

    idk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 
 import { theme } from 'styles/theme';
